6d177018467ac5716d01053b03a23582d8d80753
jaapaurelio <jaapaurelio@gmail.com>
2016-01-16 17:12:11 +0000

    Resize camera on resolution change

---
+++ Assets/Scripts/CameraResize.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraResize : MonoBehaviour {
+
+	private float targetAspect = 2.0f / 3.0f ;
+	private float originalOrthographicSize = 9.6f;
+	private float windowAspect = 0;
+
+	void Start () 
+	{
+		Resize();
+	}
+		
+	void Update() {
+		float currentWindowAspect = (float)Screen.width / (float)Screen.height;
+
+		// check if we have a new resolution
+		if(currentWindowAspect != windowAspect) {
+			Resize();
+		}
+	}
+
+	private void Resize() {
+		windowAspect = (float)Screen.width / (float)Screen.height;
+		float scaleHeight = windowAspect / targetAspect;
+		Camera camera = GetComponent<Camera>();
+
+		if (scaleHeight < 1.0f)
+		{  
+			camera.orthographicSize = originalOrthographicSize / scaleHeight;
+		}
+	}
+}
