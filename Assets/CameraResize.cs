e87dd8b9378f03380297f0a69e0f9863522678d4
jaapaurelio <jaapaurelio@gmail.com>
2016-01-16 16:51:22 +0000

    Resize camera to play in any resolution creating a letterbox  effect

---
+++ Assets/CameraResize.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraResize : MonoBehaviour {
+
+	private float targetAspect = 2.0f / 3.0f ;
+
+	void Start () 
+	{
+		float windowAspect = (float)Screen.width / (float)Screen.height;
+		float scaleHeight = windowAspect / targetAspect;
+		Camera camera = GetComponent<Camera>();
+
+		if (scaleHeight < 1.0f)
+		{  
+			camera.orthographicSize = camera.orthographicSize / scaleHeight;
+		}
+	}
+
+}
