6cc2da115b322ff6d4e6e509134642990782c2b0
Luís Duarte de Jesus Santos <luisdjsantos@gmail.com>
2016-01-31 16:58:38 +0000

    We can now use the mouse to test the game. (Fix by Luis Santos -PRO)

--- Assets/Scripts/TouchManager.cs
+++ Assets/Scripts/TouchManager.cs
@@ -3,20 +3,32 @@ using System.Collections;
 
 public class TouchManager : MonoBehaviour {
 
-	void Update() {
-		
-		if(Input.touchCount > 0 ){
-			Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+	public void Update() {
+		if(Input.touchCount > 0){
+			TriggerEvent(true);
+		}else if(Input.GetMouseButtonDown(0)){
+			TriggerEvent(false);
+		}
+	}
 
-			RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+	private static void TriggerEvent(bool mouseevent){
+		Vector3 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 
+		RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+		if (mouseevent) {
 			var touch = Input.GetTouch(0);
-			if( touch.phase == TouchPhase.Began ) {
-				if( hit.collider ) {
-					hit.collider.gameObject.SendMessage("OnTouch_TM", SendMessageOptions.DontRequireReceiver);
-				}
+			if (touch.phase == TouchPhase.Began) {
+				HitCollider(hit);
 			}
+		} else {
+			HitCollider(hit);
+		}
+	}
 
+	private static void HitCollider(RaycastHit2D hit){
+		if (hit.collider) {
+			hit.collider.gameObject.SendMessage("OnTouch_TM", SendMessageOptions.DontRequireReceiver);
 		}
 	}
 }
